7565a93e905a646b69803f981c2082de3ce5da2b
sravyasravs008 <106951281+sravyasravs008@users.noreply.github.com>
2022-08-15 12:24:28 +0530

    react

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
 import ReactDom from "react-dom"
-//ReactDom.render(<h1>hello world </h1>,document.getElementById("Message"))
+ReactDom.render("hello gud morning",document.getElementById("Message"))
+ReactDom.render(<h1>hello world </h1>,document.getElementById("Message"))
 ReactDom.render(<div>
     <h1>hello world</h1>
     <h2>welcome to react</h2>
