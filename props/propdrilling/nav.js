a79a26b4bdb35928b22f50473e46f40088daedf8
sravyasravs008 <106951281+sravyasravs008@users.noreply.github.com>
2022-08-16 16:18:01 +0530

    react

---
+++ props/propdrilling/nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+class Nav extends React.Component{
+    render(){
+        return<nav className="navbar navbar-dark bg-primary">
+        <a href="#" className="navbar-brand">props drill</a>
+        </nav>
+    }
+}
+export default Nav
