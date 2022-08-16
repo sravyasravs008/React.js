366accd62bdf80b71bb08f849200a2d7276bcf9e
sravyasravs008 <106951281+sravyasravs008@users.noreply.github.com>
2022-08-16 16:19:25 +0530

    Delete propdrilling directory

--- propdrilling/App4.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-class App extends React.Component{
-    render(){
-        return<div>
-        <h4>name={JSON.stringify(this.props)}</h4>
-        </div>
-    }
-}
-export default App
--- propdrilling/nav.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-class Nav extends React.Component{
-    render(){
-        return<nav className="navbar navbar-dark bg-primary">
-        <a href="#" className="navbar-brand">props drill</a>
-        </nav>
-    }
-}
-export default Nav
